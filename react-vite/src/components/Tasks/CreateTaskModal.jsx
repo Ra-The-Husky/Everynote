84979fcaca81a1d86a05c5463e729c84cb17831f
Ra Lin <rasheedlindsey@live.com>
2024-03-26 20:32:57 -0400

    User can create new tasks

--- react-vite/src/components/Tasks/CreateTaskModal.jsx
+++ react-vite/src/components/Tasks/CreateTaskModal.jsx
@@ -1,42 +1,80 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useModal } from "../../context/Modal";
 import DatePicker from "react-datepicker";
-import 'react-datepicker/dist/react-datepicker.css'
+import "react-datepicker/dist/react-datepicker.css";
+import { createTask } from "../../redux/tasks";
+
 
 function CreateTaskModal() {
   const { closeModal } = useModal();
 
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [name, setName] = useState("");
   const [deadline, setDeadline] = useState(new Date());
-  const [priority, setPriority] = useState();
+  const [priority, setPriority] = useState("Low");
   const [description, setDescription] = useState("");
   const [reminder, setReminder] = useState(false);
-  const [done, setDone] = useState(false);
+  const [errors, setErrors] = useState({});
+  const tasks = useSelector((state) => state.tasks.tasks)
+
+
+  const testTask = () => {
+    setName("Test Task");
+    setDescription("Setting a test description for the test task");
+  };
+
+  useEffect(() => {
+    const errs = {};
+    if (!name) {
+      errs.name = "Task name required";
+    }
+    tasks.forEach(task => {
+      if (name === task.name){
+        errs.name = "Task already exists"
+      }
+
+    });
+    if (name.length > 30) {
+      errs.name = "Cannot exceed 30 characters";
+    }
+    if (!description) {
+      errs.description = "Task description required";
+    }
+    if (description.length < 20) {
+      errs.description = "Description should be a minimum of 20 characters";
+    }
+    setErrors(errs);
+  }, [name, description]);
 
   const submitTask = async (e) => {
     e.preventDefault();
+
     const newTask = {
       name,
-      deadline,
+      'deadline': new Date(deadline).toISOString().split('T').splice(0,1).join(""),
       priority,
       description,
       reminder,
-      'status': done
     };
 
-    dispatch().then(() => {
-      closeModal()
-      navigate(`/tasks`)
-    })
+    if (Object.values(errors).length) {
+      return console.log(errors);
+    } else {
+      dispatch(createTask(newTask)).then(() => {
+        closeModal();
+
+        navigate(`/tasks`);
+      });
+    }
   };
 
-  const handleChange = date => {
-    setDeadline(date)
+  const handleChange = (date) => {
+    setDeadline(date);
   };
+
 
   return (
     <>
@@ -49,6 +87,7 @@ function CreateTaskModal() {
             value={name}
             onChange={(e) => setName(e.target.value)}
           ></input>
+          {errors.name && <p>{errors.name}</p>}
         </div>
         <div className="deadline">
           <DatePicker
@@ -57,15 +96,14 @@ function CreateTaskModal() {
             onChange={handleChange}
           />
         </div>
-        <div className="deadline">
+        <div className="priority">
           <select
             value={priority}
             onChange={(e) => setPriority(e.target.value)}
           >
-            <option
-              value={""}
-              disabled
-            >Please Select Priority</option>
+            <option value={""} disabled>
+              Please Select Priority
+            </option>
             <option value={"Low"}>Low</option>
             <option value={"Medium"}>Medium</option>
             <option value={"High"}>High</option>
@@ -78,6 +116,7 @@ function CreateTaskModal() {
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           ></textarea>
+          {errors.description && <p>{errors.description}</p>}
         </div>
         <div className="reminder">
           <label htmlFor="reminder">Reminder</label>
@@ -88,25 +127,19 @@ function CreateTaskModal() {
             onChange={(e) => setReminder(e.target.value)}
           ></input>
         </div>
-        <div className="status">
-          <label htmlFor="status">Done</label>
-          <input
-            name="status"
-            type="checkbox"
-            value={done}
-            onChange={(e) => setDone(e.target.value)}
-          ></input>
-        </div>
         <div className="buttons">
           <button
             className="button"
             type="submit"
-            disabled={!name || !deadline || !priority || !description}
-          >Save</button>
+            disabled={Object.values(errors).length}
+          >
+            Save
+          </button>
+          <button onClick={testTask}>Test Task</button>
         </div>
       </form>
     </>
-  )
+  );
 }
 
-export default CreateTaskModal
+export default CreateTaskModal;
