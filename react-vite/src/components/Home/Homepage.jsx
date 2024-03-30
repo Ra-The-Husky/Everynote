51c4f6bfeead5e14a570f7d676ca381c45057ede
Ra Lin <rasheedlindsey@live.com>
2024-03-30 12:22:55 -0400

    Merged new user branch changes

--- react-vite/src/components/Home/Homepage.jsx
+++ react-vite/src/components/Home/Homepage.jsx
@@ -1,9 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect,  useState } from "react";
+import { useEffect, useState } from "react";
 import { homeThunk } from "../../redux/home";
 import moment from "moment";
 
-import './Homepage.css'
+import "./Homepage.css";
 
 function Homepage() {
   const userNotes = useSelector((state) => state.home?.notes);
@@ -15,11 +15,7 @@ function Homepage() {
   const dispatch = useDispatch();
 
   if (userTasks && userTasks.length > 0 && tasks.length === 0) {
-    setTasks([userTasks[0], userTasks[1], userTasks[2]])
-  }
-
-  if (userNotes && userNotes.length > 0 && notes.length === 0) {
-    setNotes([userNotes[0], userNotes[1], userNotes[2]])
+    setTasks([userTasks[0], userTasks[1], userTasks[2]]);
   }
 
   if (userNotes && userNotes.length > 0 && notes.length === 0) {
@@ -30,57 +26,42 @@ function Homepage() {
     dispatch(homeThunk());
   }, [dispatch]);
   return (
-    <div className="HomePage">
+    <>
       <img src="https://res.cloudinary.com/dfxxgifho/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710376011/istockphoto-1303583671-612x612_f5tvml.jpg?_s=public-apps" />
       <h2>Home</h2>
       <div className="homeContent">
-
         <div className="homeLeftPanel">
           <div className="homeNotebooks">
             <h4>{userNotebooks && userNotebooks[0]?.name}</h4>
             <p>{userNotebooks && userNotebooks[0]?.description}</p>
           </div>
-
           <div className="homeTasks">
             <h3>Your Tasks</h3>
-              {tasks &&
-                tasks.map((task) => (
-                  <div className="homeTask" key={task.id}>
-                    <div className="taskInfo">
-                      <p>
-                      {task.name}
-                      </p>
-                      <p>
-                      {moment(task.deadline).format("MM-DD-YYYY")}
-                      </p>
-                      <p>
-                      {task.priority}
-                      </p>
-                    </div>
+            {tasks &&
+              tasks.map((task) => (
+                <div className="homeTask" key={task.id}>
+                  <div className="taskInfo">
+                    <p>{task.name}</p>
+                    <p>{moment(task.deadline).format("MM-DD-YYYY")}</p>
+                    <p>{task.priority}</p>
                   </div>
-                ))}
+                </div>
+              ))}
           </div>
         </div>
-
-        <div className="homeNotes">
-          {userNotes &&
-            userNotes.map((note) => (
-              <div className="homeNote" key={note.id}>
-                <div className="noteInfo">
-                  <h4>{note.name}</h4>
-                  <p>{note.info}</p>
-                </div>
-                <div className="homeTags">
-                  {note.tags && note.tags.map((tag) => (
-                    <div>
-                      {tag.name}
-                    </div>
-                  ))}
-                </div>
+        {userNotes &&
+          userNotes.map((note) => (
+            <div className="homeNote" key={note.id}>
+              <div className="noteInfo">
+                <h4>{note.name}</h4>
+                <p>{note.info}</p>
               </div>
-            ))}
-      <h2>Users Home</h2>
-      <div>
+              <div className="homeTags">
+                {note.tags && note.tags.map((tag) => <div>{tag.name}</div>)}
+              </div>
+            </div>
+          ))}
+        <h2>Users Home</h2>
         <div>{userNotebooks && userNotebooks[0]?.name}</div>
         <p>{userNotebooks && userNotebooks[0]?.description}</p>
         <div>
@@ -123,9 +104,8 @@ function Homepage() {
             )}
           </div>
         </div>
-
       </div>
-    </div>
+    </>
   );
 }
 
