168debde0f1c46747224268a22d48fd9fff26f33
Andrew McCord <a2011mccord@gmail.com>
2024-03-30 15:00:21 -0400

    edit note page styled

--- react-vite/src/components/Notes/NoteDetails.jsx
+++ react-vite/src/components/Notes/NoteDetails.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { destroyTag, noteInfo } from "../../redux/notes";
+import { destroyTag, editNote, noteInfo } from "../../redux/notes";
 import OpenModalButton from "../OpenModalButton/OpenModalButton";
 import DeleteNoteModal from "./DeleteNoteModal";
 import { notebookThunk } from "../../redux/notebooks";
@@ -15,52 +15,52 @@ function NoteInfo() {
   const notebook = notebooks?.find((notebook) => notebook.id === noteDetails?.notebook_id);
   const dispatch = useDispatch();
 
-  const edit = (e) => {
-    e.preventDefault();
-    navigate(`/notes/${noteId}/edit`);
-  };
-
   useEffect(() => {
     dispatch(noteInfo(Number(noteId)));
     dispatch(notebookThunk());
   }, [dispatch, noteId]);
 
   return (
-    <>
-      <h5>{notebook?.name}</h5>
+    <div className="editNoteCont">
+      <h4>Notebook: {notebook?.name}</h4>
       <h1 className="name-of-note">{noteDetails?.name}</h1>
-      <div>{noteDetails?.caption}</div>
-      <span className="detail-of-note">
-      <p>{noteDetails?.info}</p>
-      <div>
+      <div className="editNoteCaption">{noteDetails?.caption}</div>
+
+      <div className="editNoteContent">
+        <p>{noteDetails?.info}</p>
+      </div>
+
+      <div className="editNoteFooter">
         {!tags?.length ? (
           <></>
         ) : (
-          <div>
-            {tags &&
-              tags.map((allTags) =>
-                allTags.map((tag) => (
-                  <div key={tag.id}>
-                    #{tag.name}{" "}
-                    <div>
-                      {" "}
-                      <i
-                        className="fa-solid fa-square-xmark"
-                        onClick={() => dispatch(destroyTag(noteId, tag.id))}
-                      ></i>
-                    </div>
+          <div className = "editNoteTags">
+              {
+            tags &&
+            tags.map((allTags) =>
+              allTags.map((tag) => (
+                <div className="editNoteTag" key={tag.id}>
+                  #{tag.name}{" "}
+                  <div>
+                    {" "}
+                    <i
+                      className="fa-solid fa-square-xmark"
+                      onClick={() => dispatch(destroyTag(noteId, tag.id))}
+                    ></i>
                   </div>
-                ))
-              )}
-          </div>
-        )}
+                </div>
+              ))
+            )
+          }
+            </div>
+          )}
+      <div className="editNoteButtons">
+        <i className="fa-regular fa-pen-to-square" onClick={() => navigate(`/notes/${noteId}/edit`)}></i>
+        <OpenModalButton modalComponent={<DeleteNoteModal noteId={noteId} />} />
       </div>
-      <div>
-        <button onClick={edit}>Edit Note</button>
-      </div>
-      <OpenModalButton modalComponent={<DeleteNoteModal noteId={noteId} />} />
-      </span>
-    </>
+    </div>
+
+    </div >
   );
 }
 
