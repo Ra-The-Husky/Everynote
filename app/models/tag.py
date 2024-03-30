2fdc071b14473dcb4ef1cb5a7f0bc7c7bf1e73dd
Ra Lin <rasheedlindsey@live.com>
2024-03-30 17:50:45 -0400

    db production adjustment

--- app/models/tag.py
+++ app/models/tag.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Tag(db.Model):
     __tablename__ = 'tags'
@@ -7,8 +7,8 @@ class Tag(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    note_id = db.Column(db.Integer, db.ForeignKey('notes.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    note_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('notes.id')), nullable=False)
     name = db.Column(db.String(50), nullable=False)
 
     user = db.relationship('User', back_populates='tags')
--- app/models/task.py
+++ app/models/task.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Task(db.Model):
     __tablename__ = 'tasks'
@@ -7,7 +7,7 @@ class Task(db.Model):
           __table_args__= {'schema':SCHEMA}
 
     id = db.Column(db.Integer,primary_key=True)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey(add_prefix_for_prod('users.id')),nullable = False)
     name = db.Column(db.String(50), nullable=False)
     deadline= db.Column(db.Date,nullable = False)
     priority = db.Column(db.String(50),nullable = False)
