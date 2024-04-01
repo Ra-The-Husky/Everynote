e1a8377d6210e180457f35d1e16250b67b60ef64
Mohammad Najad <mjlr0909@gmail.com>
2024-04-01 14:05:57 -0400

    about page

--- react-vite/src/components/Navigation/AboutModal.jsx
+++ react-vite/src/components/Navigation/AboutModal.jsx
@@ -1,20 +1,50 @@
 // import { useModal } from "../../context/Modal";
+import "./AboutModal.css";
 
 function AboutModal() {
   return (
     <div className="about-modal">
       <h2>EVERYNOTE</h2>
+      <p>
+        Everynote is a useful application that lets users create and manage
+        notes and tasks efficiently. With Everynote, users have the ability to
+        organize their notes into different notebooks, allowing for a
+        personalized and organized note-taking experience tailored to their
+        preferences. We understand that most people prefer to keep their notes
+        and tasks for themselves, but not in a way that results in cluttered and
+        disorganized files. That's why we developed Everynote—to provide a
+        solution that helps users maintain their notes and tasks in a structured
+        and user-friendly manner. Whether you're jotting down ideas, creating
+        to-do lists, or organizing thoughts, Everynote offers the tools you need
+        to keep everything neatly organized and easily accessible.
+      </p>
       <h4>Created by:</h4>
-      <h3>Andrew McCord</h3>
-      <a href="https://github.com/a2011mccord">a2011mccord</a>
-      <h3>Zack Stephens</h3>
-      <a href="https://github.com/ZackStephens92">ZackStephens92</a>
-      <h3>Rasheed Lindsey</h3>
-      <a href="https://github.com/Ra-The-Husky">Ra-The-Husky</a>
-      <h3>Mohammad Najad</h3>
-      <a href="https://github.com/Mjcoco09">Mjcoco09</a>
+      <div className="about-modal">
+        <h3>Andrew McCord</h3>
+        <a href="https://github.com/a2011mccord">a2011mccord</a>
+
+        <h3>Zack Stephens</h3>
+        <a href="https://github.com/ZackStephens92">ZackStephens92</a>
+
+        <h3>Rasheed Lindsey</h3>
+        <a href="https://github.com/Ra-The-Husky">Ra-The-Husky</a>
+
+        <h3>Mohammad Najad</h3>
+        <a href="https://github.com/Mjcoco09">Mjcoco09</a>
+
+        <p>
+          For feedback feel free to
+          contact us via email or connect with us on GitHub.
+        </p>
+
+        <h4>Get in Touch:</h4>
+        <p>Email:comingSoon@noEmail.com</p>
+        <a href="https://github.com/Ra-The-Husky/Everynote/">
+          Everynote GitHub
+        </a>
+      </div>
     </div>
   );
 }
 
-export default AboutModal
+export default AboutModal;
